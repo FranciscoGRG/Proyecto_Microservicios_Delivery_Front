559680117f69e313f303982ea650e7f5a4c628c8
Fran <fgrcalifa@gmail.com>
2025-11-06 18:34:31 +0100

    Html de Navbar y footer, faltaria cambiar los href por routerLink

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './navbar.html',
 })
 export class NavbarComponent {
+
+  isMobileMenuOpen: boolean = false;
+  isProfileMenuOpen: boolean = false;
+
+  toogleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+
+    if(this.isMobileMenuOpen) {
+      this.isProfileMenuOpen = false;
+    }
+  }
+
+  toogleProfileMenu() {
+    this.isProfileMenuOpen = !this.isProfileMenuOpen;
+
+    if(this.isProfileMenuOpen) {
+      this.isMobileMenuOpen = false;
+    }
+  }
 
 }
