fdbc372df678424461e0ceeb03a70b386c4fa08b
Fran <fgrcalifa@gmail.com>
2025-11-06 14:44:00 +0100

    Primer comit, creado los componentes de navbar, landing y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './components/landing-page/landing-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: LandingPageComponent, pathMatch: 'full'},
+
+
+    {path: '**', redirectTo: ''}
+];
