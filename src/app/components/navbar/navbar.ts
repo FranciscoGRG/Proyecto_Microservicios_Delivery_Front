f3a73c45ab97753ff9da9281dce1bc9b8a481666
Fran <fgrcalifa@gmail.com>
2025-11-10 17:56:15 +0100

    Cambiado el icono de notificaciones del navbar por uno para mostrar el carro y la cantidad de items que contiene, estes logeado o no.

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,23 +1,32 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../services/auth-service';
 import { Router, RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart-service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   imports: [CommonModule, RouterLink],
   templateUrl: './navbar.html',
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   authService = inject(AuthService);
+  private cartService = inject(CartService);
   private router = inject(Router);
   isMobileMenuOpen: boolean = false;
   isProfileMenuOpen: boolean = false;
 
+  cartItemCount$!: Observable<number>;
+
+  ngOnInit(): void {
+    this.cartItemCount$ = this.cartService.getTotalItemsCount();
+  }
+
   toogleMobileMenu() {
     this.isMobileMenuOpen = !this.isMobileMenuOpen;
 
-    if(this.isMobileMenuOpen) {
+    if (this.isMobileMenuOpen) {
       this.isProfileMenuOpen = false;
     }
   }
@@ -25,16 +34,15 @@ export class NavbarComponent {
   toogleProfileMenu() {
     this.isProfileMenuOpen = !this.isProfileMenuOpen;
 
-    if(this.isProfileMenuOpen) {
+    if (this.isProfileMenuOpen) {
       this.isMobileMenuOpen = false;
     }
   }
 
   logout(): void {
-    this.authService.logout()
+    this.authService.logout();
     this.isMobileMenuOpen = false;
     this.isProfileMenuOpen = false;
     this.router.navigate(['/products']);
   }
-
 }
